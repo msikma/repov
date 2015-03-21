e334e4e4d432b4e9ef2452d9e3df15498853ffef
Michiel Sikma <michiel@privax.com>
2015-03-21 01:12:59 +0000

    Bump to 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='repov',
     packages=['repov'],  # this must be the same as the name above
-    version='0.1',
+    version='0.2',
     description='Retrieves Git repo branch information synchronously',
     author='Michiel Sikma',
     author_email='michiel@sikma.org',
     url='https://github.com/msikma/repov',
-    download_url='https://github.com/msikma/repov/tarball/0.1',
+    download_url='https://github.com/msikma/repov/tarball/0.2',
     keywords=['git', 'repository', 'branch', 'information'],
     classifiers=[],
 )
