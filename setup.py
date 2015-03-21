1dbb9a7aa4f051ffa362fa7b6fa465b115d8957a
Michiel Sikma <michiel@privax.com>
2015-03-21 01:12:34 +0000

    Added setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='repov',
+    packages=['repov'],  # this must be the same as the name above
+    version='0.1',
+    description='Retrieves Git repo branch information synchronously',
+    author='Michiel Sikma',
+    author_email='michiel@sikma.org',
+    url='https://github.com/msikma/repov',
+    download_url='https://github.com/msikma/repov/tarball/0.1',
+    keywords=['git', 'repository', 'branch', 'information'],
+    classifiers=[],
+)
